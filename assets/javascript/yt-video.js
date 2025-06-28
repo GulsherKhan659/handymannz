4311128962b5996678e8738c091c1a35bcb07e0d
GulsherKhan659 <kzai659@yahoo.com>
2025-06-28 11:35:42 +0500

    6/27/2025

--- assets/javascript/yt-video.js
+++ assets/javascript/yt-video.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
     $('.youtube').magnificPopup({
         items: {
-            src: 'https://www.youtube.com/watch?v=Hh3MjLaDNG8'
+            src: 'https://www.youtube.com/watch?v=FL9VJg_CpYc'
         },
         type: 'iframe'
     });
